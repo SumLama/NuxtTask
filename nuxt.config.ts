05952e113f1a8de859a6c7f8fc7f94d662075a15
sumitra tamang <sumi8tamang@gmail.com>
2024-09-15 18:03:02 +0545

    feat: project setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  components: true,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+  modules: ['@nuxt/ui']
 })

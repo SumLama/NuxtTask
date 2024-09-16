15ab04219cb454c27647e9320e1badfa7b834941
sumitra tamang <sumi8tamang@gmail.com>
2024-09-16 18:05:09 +0545

    feat: create news details  page and api integration

---
+++ pages/news/[id].vue
@@ -0,0 +1,39 @@
+<script setup>
+const route = useRoute();
+const apiUrl = `https://swa-2024-dev.up.railway.app/api/media-center/news/${route.params.id}`;
+const { data: newsDetails, status } = await useFetch(apiUrl);
+console.log(newsDetails)
+</script>
+
+<template>
+  <div>
+    <div v-if="status === 'pending'">
+      <p>Loading......</p>
+    </div>
+   
+    <div v-else-if="status === 'success'">
+      <div class="w-4/6 mx-auto py-16 xl:py-32">
+        <div
+          v-for="item in newsDetails"
+          :key="item.id"
+          class="space-y-5"
+        >
+          <div>
+            <p class="text-medium text-3xl">{{ item.title }}</p>
+          </div>
+          <div class="mt-1 mb-14">
+            <span>{{ item.createdAt }}</span
+            ><br />
+            <span>{{ item.readTime }} min read</span>
+          </div>
+          <div class="my-5">
+            <img :src="item.image" :alt="item.title" class="h-[55%] w-[55%]" />
+          </div>
+          <div>
+            <p class="leading-loose">{{ item.content }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

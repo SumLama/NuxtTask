db0b3872b4956a30dbc3f4fa30b65679c63bf36c
sumitra tamang <sumi8tamang@gmail.com>
2024-09-16 10:11:02 +0545

    feat: add countries as array of object and change color of background image

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,4 @@
 <script setup>
-const countries = ['United States', 'Canada', 'Mexico']
-
-const country = ref(countries[0])
 const navItems =[
   { route: '/', label: 'Home' },
   { route: '/agenda', label: 'Agenda' },
@@ -13,16 +10,28 @@ const navItems =[
   { route: '/contactus', label: 'Contact us' },
 
 ]
+const items = [
+  [{
+    label: 'English',
+    
+  }], [{
+    label: 'Arabian',
+    
+  }, {
+    label: 'English',
+    
+  }]
+]
 </script>
 
 <template>
-    <div class="bg-[url('/assets/images/header-bg.jpg')] h-72 bg_image bg-cover">
+    <div class="bg-[url('/assets/images/header3.jpg')]  bg_image bg-cover">
     <div class="mx-20">
         <div>
-            <ul class="flex justify-end items-center space-x-10  text-bold text-white">
+            <ul class="flex justify-end items-center space-x-5 font-bold text-sm text-white py-3">
             <li>
                 <NuxtLink to="/register" class="relative flex items-center">
-                <label class="mr-2">Register now</label>
+                <label class="mr-2 ">Register now</label>
                 <UIcon name="heroicons:arrow-long-right" class="w-5 h-5" />
                 </NuxtLink>
             </li>
@@ -33,9 +42,9 @@ const navItems =[
                 </NuxtLink>
             </li>
             <li>
-                <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">   
-                 <UButton color="white" class="ring-0  text-bold text-base" label="English" trailing-icon="i-heroicons-chevron-down-20-solid" />
-                </UDropdown>
+                <UDropdown v-model:open="open" :items="items" :popper="{ placement: 'bottom-start' }">
+    <UButton color="" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" class="font-bold ring-0 text-sm bg-transparent hover:ring-0" />
+  </UDropdown>
             </li>
             </ul>
         </div>
@@ -43,20 +52,23 @@ const navItems =[
             <div class=" flex-col justify-around  w-1/3">
         
         <div>
-          <img src="/assets/images/Logo.jpg" alt="Logo"/>
+          <img src="/assets/images/new_logo.png" alt="Logo">
+        </div>
+        <div >
+            <h1 class="text-5xl font-bold text-white align-text-bottom my-12 pl-5 ">Media Center</h1>
         </div>
       </div>
             <div class="flex-1">
-                <ul class="flex justify-between items-center space-x-2 text-bold text-white">
+                <ul class="flex justify-between items-center space-x-2 font-bold text-sm text-white">
                     <li v-for="(item, index) in navItems" :key="index">
                         <NuxtLink :to="item.route">
                         <span >{{ item.label }}</span>
                         </NuxtLink>
                     </li>
                     <li>
-                        <UButton variant="solid" class="button bg-sky-600 px-8 rounded-none">
+                        <UButton variant="solid" class="button bg-sky-600  hover:bg-sky-600 px-8 rounded-none">
                             <UIcon name="material-symbols:auto-videocam-outline-sharp" class="w-5 h-5" />
-                            <span class="text-bold">LIVE STREAMING</span>
+                            <span class="text-sm font-bold">LIVE STREAMING</span>
                         </UButton>
                     </li>
                 </ul>
@@ -67,7 +79,9 @@ const navItems =[
 </template>
 <style scoped>
 .bg_image{
-    background-position: center 75%; 
+    background-position:  center 75%; 
+  
+  
 }
 .button{
   clip-path: polygon(6% 0%, 100% 0%, 94% 100%, 0% 100%);

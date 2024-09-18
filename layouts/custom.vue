b397eac7effb4eb5b92763acbc37e35bc81be3e2
sumitra tamang <sumi8tamang@gmail.com>
2024-09-18 17:40:36 +0545

    feat: create custom layout for newsdetails header

---
+++ layouts/custom.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+<template>
+<div>
+ <header>
+    <slot name="header"></slot>
+ </header>  
+ <main>
+    <slot/>
+ </main>
+ <footer>
+    <Footer/>
+ </footer> 
+</div>
+
+</template>

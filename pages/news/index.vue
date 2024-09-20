2b1ac9544ba2bd41c84e6003da74ce6c73ef5e56
sumitra tamang <sumi8tamang@gmail.com>
2024-09-20 18:02:13 +0545

    feat: use data fns formatter for date and time, use vhtml

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,20 +1,20 @@
 <script setup>
-const {data:news,status} = await getNews().newsList();
+import { format } from 'date-fns';
+const {data:news, status } = await useFetch('https://swa-2024-dev.up.railway.app/api/media-center/news');
 </script>
 <template>
   <div>
-    <div v-if="status === 'pending'"><p>Loading data........</p></div>
-    <div v-else-if="status === 'error'"><p>Fetching error</p></div>
-    <div v-else-if="status === 'success'" >
+    <div v-if="status ==='pending'"><p>Loading data........</p></div>
+    <div v-else-if="status ==='error'"><p>Fetching error</p></div>
+    <div v-else-if="status ==='success'" >
       <div class="lg:w-4/6 mx-auto px-8 md:px-16 lg:px-0">
         <h1 class="text-3xl font-bold my-3">Featured News</h1>
-     
           <UCarousel
             fix
             :items="news.results"
             :ui="{
           item: 'basis-full md:basis-1/2',
-              container: 'rounded-none gap-4 h-full  ',
+              container: 'rounded-none gap-4  ',
               arrows: {
                 wrapper: 'flex items-center justify-end my-2 ',
                 prev: 'mr-2',
@@ -25,19 +25,23 @@ const {data:news,status} = await getNews().newsList();
             >
             <template #default="{ item }">
               <div class="flex flex-col w-full px-2">
+                <div class=" w-full h-[270px] sm:h-[300px] xl:h-[355px]">
                 <NuxtLink :to="`/news/${item.id}`">
-        
+               
+
+              
                   <img
-                    :src="joinUrls(item.image)"
+                    :src="getImageUrl(item.image)"
                     :alt="item.title"
-                    class="w-full h-[260px]  md:h-[220px] xl:h-[325px] object-cover snap-start "
+                    class="w-full h-full object-cover snap-start "
                     draggable="false"
                   />
            
                 </NuxtLink>
+                </div>
                 <div class="mt-4">
                   <div class="flex text-xs space-x-3">
-                    <span>{{ item.createdAt }}</span>
+                    <span>{{ format(new Date(item.createdAt), 'dd MMM yyyy, hh.mm aa') }}</span>
                     <span class="flex space-x-2">
                       <UIcon name="heroicons-outline:clock" class="w-4 h-4" />
                       <span>{{ item.readTime }} Min Read</span>
@@ -90,13 +94,13 @@ const {data:news,status} = await getNews().newsList();
           >
             <li>
               <img
-                :src="joinUrls(item.image)"
+                :src="getImageUrl(item.image)"
                 :alt="item.title"
                 class="w-full md:h-56 lg:h-48 object-cover"
               />
             </li>
             <li class="flex space-x-4 text-xs">
-              <span class="text-xs">{{ item.createdAt }}</span>
+              <span>{{ format(new Date(item.createdAt), 'dd MMM yyyy, hh.mm aa') }}</span>
               <span class="flex items-center space-x-2 text-xs">
                 <UIcon name="heroicons-outline:clock" class="w-4 h-4" />
                 <span>{{ item.readTime }} Min Read</span>

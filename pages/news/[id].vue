df4265679a61539c0ca789246466e8020ef8853e
sumitra tamang <sumi8tamang@gmail.com>
2024-09-22 20:42:57 +0545

    feat: create interface defining response structure, use of interface for news deatils

--- pages/news/[id].vue
+++ pages/news/[id].vue
@@ -1,10 +1,27 @@
-<script setup>
+<script setup lang="ts">
 import { format } from 'date-fns';
+
 definePageMeta({
   layout: false,
 });
+interface NewsDetail {
+  id: number;
+  title: string;
+  createdAt: string;
+  readTime: number;
+  content: string;
+  image: string;
+}
+
+interface DetailResponse {
+  pagination: any,
+  details: NewsDetail; 
+}
 const route = useRoute();
-const {data:item,status} =  await useFetch(`https://swa-2024-dev.up.railway.app/api/media-center/news/${route.params.id}`);      
+const { data: item, status } = await useFetch<DetailResponse>(
+  `https://swa-2024-dev.up.railway.app/api/media-center/news/${route.params.id}`
+);
+
 </script>
 <template>
   <div>
@@ -16,20 +33,20 @@ const {data:item,status} =  await useFetch(`https://swa-2024-dev.up.railway.app/
         <p>Loading......</p>
       </div>
 
-      <div v-else-if="status === 'success'">
+      <div v-else-if="status === 'success' && item">
         <div class="w-4/6 mx-auto py-16 xl:py-32 space-y-5">
           <div>
-            <p class="text-medium text-3xl">{{ item.title }}</p>
+            <p class="text-medium text-3xl">{{ item.details.title }}</p> <!-- Access 'details' -->
           </div>
           <div class="mt-1 mb-14">
-            <span>{{format(new Date(item.createdAt), 'dd MMM yyyy, hh.mm aa')}}</span>
-            <br/>
-            <span>{{ item.readTime }} min read</span>
+            <span>{{ format(new Date(item.details.createdAt), 'dd MMM yyyy, hh.mm aa') }}</span>
+            <br />
+            <span>{{ item.details.readTime }} min read</span>
           </div>
           <div class="my-5 w-full lg:w-[450px]">
-            <img :src="getImageUrl(item.image)" :alt="item.title" class="w-full"/>
+            <img :src="getImageUrl(item.details.image)" :alt="item.details.title" class="w-full" />
           </div>
-          <div v-html="item.content"></div>
+          <div v-html="item.details.content"></div>
         </div>
       </div>
     </NuxtLayout>
